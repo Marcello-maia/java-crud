02622f677ab453b00f9cde486cdf573fd0db701e
Marcello Maia Garcia <marcellomaiagarcia@gmail.com>
2023-11-07 20:16:46 -0300

    feature: innitial structure

---
+++ src/test/java/br/com/java/javacrud/javacrud/JavaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.javacrud.javacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
